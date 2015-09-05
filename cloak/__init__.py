d77d753a239ea63bdd4e26e697cc188839f5e940
Lori Culberson <bigskybandit@sbcglobal.net>
2015-09-05 16:31:52 -0600

    Create Url model

---
+++ cloak/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'cloak.apps.CloakConfig'
---
+++ cloak/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils import timezone
+
+class Url(models.Model):
+  user_added_url = models.URLField(max_length=200)
+  obfuscated_url = models.URLField(max_length=200)
+
+  def __str__(self):
+    return self.user_added_url
+
+  def __str__(self):
+    return self.obfuscated_url
