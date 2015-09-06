c3b0b9cfaf9a73b50aa59e5e6f91369b84691078
Lori Culberson <bigskybandit@sbcglobal.net>
2015-09-06 09:13:08 -0600

    Add click counter to Url model

--- cloak/admin.py
+++ cloak/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Url
 
 class UrlAdmin(admin.ModelAdmin):
-  list_display = ('user_added_url','obfuscated_url')
+  list_display = ('user_added_url','obfuscated_url', 'obfuscated_url_click_counter')
 
 admin.site.register(Url, UrlAdmin)
 # Register your models here.
--- cloak/views.py
+++ cloak/views.py
@@ -5,3 +5,6 @@ from .models import Url
 def index(request):
   return render(request, 'cloak/index.html')
 
+def new_url(request):
+  return render(request, 'cloak/new_url.html')
+
