d2f278cc01ff0b79028b20207bc99085ca7d6fb6
Lori Culberson <bigskybandit@sbcglobal.net>
2015-09-06 00:18:24 -0600

    Set up admin views

--- cloak/admin.py
+++ cloak/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Url
+
+class UrlAdmin(admin.ModelAdmin):
+  list_display = ('user_added_url','obfuscated_url')
+
+admin.site.register(Url, UrlAdmin)
 # Register your models here.
