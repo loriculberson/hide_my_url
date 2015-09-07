e910c74b575de5b4329e241ee050a5ec4a7e92aa
Lori Culberson <bigskybandit@sbcglobal.net>
2015-09-06 18:20:50 -0600

    Add http to original urls that do not have it

--- cloak/views.py
+++ cloak/views.py
@@ -8,7 +8,13 @@ def index(request):
 
 def new_url(request):
   encrypted_code = 'random123'
-  original_url = 'http://' + request.POST['user_added_url']
+  original_url = request.POST['user_added_url']
+  
+  if original_url.startswith('http://'):
+    original_url = original_url
+  else:
+    original_url = 'http://' + original_url 
+
   encrypted_url = original_url + '/' + encrypted_code
   final_url = Url(user_added_url = original_url, obfuscated_url = encrypted_url)
   final_url.save()
