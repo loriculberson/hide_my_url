edd812d37e2804e5b96f9e0c52899b5f7b177e64
Lori Culberson <bigskybandit@sbcglobal.net>
2015-09-08 00:47:03 -0600

    Obfuscated links do not reveal actual link

--- cloak/admin.py
+++ cloak/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from .models import Url
 
 class UrlAdmin(admin.ModelAdmin):
-  list_display = ('user_added_url','obfuscated_url', 'obfuscated_url_click_counter')
+  list_display = ('user_added_url','obfuscated_key', 'obfuscated_url_click_counter')
 
 admin.site.register(Url, UrlAdmin)
